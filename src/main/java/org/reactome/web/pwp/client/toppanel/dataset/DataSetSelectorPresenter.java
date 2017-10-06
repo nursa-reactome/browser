760b36fc28d822313d99c650db9f68bb0c72c6aa
FredLoney <loneyf@ohsu.edu>
2017-10-06 12:21:21 -0700

    Mock dataset selection with a typical DOI.

--- src/main/java/org/reactome/web/pwp/client/toppanel/dataset/DataSetSelectorPresenter.java
+++ src/main/java/org/reactome/web/pwp/client/toppanel/dataset/DataSetSelectorPresenter.java
@@ -2,17 +2,17 @@ package org.reactome.web.pwp.client.toppanel.dataset;
 
 import com.google.gwt.event.shared.EventBus;
 
-import org.reactome.web.pwp.client.common.events.DatasetSelectedEvent;
+import org.reactome.web.pwp.client.common.events.DataSetSelectedEvent;
 
 /**
  * @author Fred Loney <loneyf@ohsu.edu>
  */
-public class DatasetSelectorPresenter implements DatasetSelector.Presenter {
+public class DataSetSelectorPresenter implements DataSetSelector.Presenter {
 
-    private DatasetSelector.Display display;
+    private DataSetSelector.Display display;
     private EventBus eventBus;
     
-    public DatasetSelectorPresenter(EventBus eventBus, DatasetSelector.Display display) {
+    public DataSetSelectorPresenter(EventBus eventBus, DataSetSelector.Display display) {
         this.eventBus = eventBus;
         this.display = display;
         this.display.setPresenter(this);
@@ -20,8 +20,8 @@ public class DatasetSelectorPresenter implements DatasetSelector.Presenter {
 
     @Override
     public void search() {
-        String datasetId = "TODO - get from text box"; 
-        DatasetSelectedEvent event = new DatasetSelectedEvent(datasetId);
+        String doi = "10.1621/gTqItVnDEP"; 
+        DataSetSelectedEvent event = new DataSetSelectedEvent(doi);
         eventBus.fireEventFromSource(event, this);
     }
 }

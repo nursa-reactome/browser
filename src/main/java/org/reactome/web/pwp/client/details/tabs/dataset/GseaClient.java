264660004c5a6d04419e15d347f04659c0cc60cb
FredLoney <loneyf@ohsu.edu>
2017-11-22 12:10:25 -0800

    Add min and max GSEA analyse parameters.

--- src/main/java/org/reactome/web/pwp/client/details/tabs/dataset/GseaClient.java
+++ src/main/java/org/reactome/web/pwp/client/details/tabs/dataset/GseaClient.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.QueryParam;
 
 import org.fusesource.restygwt.client.MethodCallback;
 import org.fusesource.restygwt.client.RestService;
@@ -13,5 +14,10 @@ import org.reactome.gsea.model.AnalysisResult;
 public interface GseaClient extends RestService {
     @POST
     @Path("/analyse")
-    public void analyse(List<List<String>> rankedList, MethodCallback<List<AnalysisResult>> result);
+    public void analyse(
+            List<List<String>> rankedList,
+            @QueryParam("dataSetSizeMin") Integer dataSetSizeMin,
+            @QueryParam("dataSetSizeMax") Integer dataSetSizeMax,
+            MethodCallback<List<AnalysisResult>> result
+    );
 }

ee49404dc9b08dba4c66b6bc0ee6e5bb2a42672a
FredLoney <loneyf@ohsu.edu>
2017-11-30 22:25:59 -0800

    Handle dataset load directly.

--- src/main/java/org/reactome/web/pwp/client/details/tabs/dataset/DataSetTabPresenter.java
+++ src/main/java/org/reactome/web/pwp/client/details/tabs/dataset/DataSetTabPresenter.java
@@ -1,16 +1,11 @@
 package org.reactome.web.pwp.client.details.tabs.dataset;
 
 import java.io.IOException;
-//import org.apache.commons.collections4.IteratorUtils;
-//import org.apache.commons.collections4.Transformer;
-//import org.apache.commons.collections4.iterators.TransformIterator;
 import org.fusesource.restygwt.client.Defaults;
 import org.fusesource.restygwt.client.Method;
 import org.fusesource.restygwt.client.MethodCallback;
-import org.reactome.web.pwp.client.common.events.DataSetLoadedEvent;
 import org.reactome.web.pwp.client.common.events.DataSetSelectedEvent;
 import org.reactome.web.pwp.client.common.events.StateChangedEvent;
-import org.reactome.web.pwp.client.common.handlers.DataSetLoadedHandler;
 import org.reactome.web.pwp.client.common.handlers.DataSetSelectedHandler;
 import org.reactome.nursa.model.DataSet;
 
@@ -20,24 +15,23 @@ import com.google.gwt.event.shared.EventBus;
 /**
  * @author Fred Loney <loneyf@ohsu.edu>
  */
-public class DataSetTabPresenter
-implements DataSetTab.Presenter, DataSetSelectedHandler, DataSetLoadedHandler {
+public class DataSetTabPresenter implements DataSetTab.Presenter, DataSetSelectedHandler {
 
     private DataSetTab.Display display;
-    private EventBus eventBus;
 
     // Initialize the resty context root. This ensures that requests
     // go to /NursaContent/... rather than /Browser/NursaContent/...
+    //
+    // Note: if additional widgets use a resty helper, then it would
+    // be cleaner to move this initializer to a global init class.
     static {
         Defaults.setServiceRoot("/");
     }
     
     public DataSetTabPresenter(EventBus eventBus, DataSetTab.Display display) {
-        this.eventBus = eventBus;
         this.display = display;
         this.display.setPresenter(this);
         eventBus.addHandler(DataSetSelectedEvent.TYPE, this);
-        eventBus.addHandler(DataSetLoadedEvent.TYPE, this);
     }
 
     @Override
@@ -52,21 +46,13 @@ implements DataSetTab.Presenter, DataSetSelectedHandler, DataSetLoadedHandler {
         getDataset(doi);
     }
 
-    @Override
-    public void onDataSetLoaded(DataSetLoadedEvent event) {
-        DataSet dataset = event.getDataSet();
-        this.display.showDetails(dataset);
-    }
-
     private void getDataset(String doi) {
         NursaClient client = GWT.create(NursaClient.class);
-        final EventBus eventBus = this.eventBus;
         client.getDataset(doi, new MethodCallback<DataSet>() {
             
             @Override
             public void onSuccess(Method method, DataSet dataset) {
-                DataSetLoadedEvent event = new DataSetLoadedEvent(dataset);
-                eventBus.fireEventFromSource(event, this);
+                display.showDetails(dataset);
             }
             
             @Override
